6801c3909b679bb7065d50e363c0ca0635910d6b
Gokay Soysal <agokaysoysal@gmail.com>
2022-04-08 10:09:28 +0300

    body mass index

---
+++ src/BmiCalculator.java
@@ -0,0 +1,23 @@
+import  java.util.Scanner;
+
+public class BmiCalculator {
+    public static void main(String[] args) {
+
+
+        float height, weight;
+        double  bodyMassIndex;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter your height in meters : ");
+        height = input.nextFloat();
+
+        System.out.print("Please enter your weight in kg : ");
+        weight = input.nextFloat();
+
+        bodyMassIndex = weight / (height * height) ;
+
+        System.out.println("Your body mass index is : " + bodyMassIndex);
+
+    }
+}
