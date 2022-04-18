ba4b0c0fb6a40a959732afc0f8a1f11cac5b4cf0
Gokay Soysal <agokaysoysal@gmail.com>
2022-04-18 12:51:49 +0300

    üssü sayılar

---
+++ src/NumbersFor.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NumbersFor {
+
+    public static void main(String[] args) {
+        int i, k, total =1;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ussu alinacak sayiyi girin: ");
+        i = input.nextInt();
+        System.out.print("Us olacak sayiyi girin: ");
+        k = input.nextInt();
+
+        for (int y = 1; y<=k;y++){
+            total *=i;
+        }
+        System.out.print(total);
+
+    }}
