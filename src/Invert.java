60ca6512cede01ff623ebf764919e5b2c850dfdc
Gokay Soysal <agokaysoysal@gmail.com>
2022-04-26 11:05:52 +0300

    inverted triangle

---
+++ src/Invert.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Invert {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n =10;
+
+        for (int i=(n-1);i>=0;i--){
+            for (int k = 0; k<(n-i); k++){
+                System.out.print(" ");
+            }
+            for (int j=0;j<(2 * i - 1); j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
