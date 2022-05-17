6a7ca5d77a8bca5e29ea006b80b2648e84b10dc8
Gokay Soysal <agokaysoysal@gmail.com>
2022-05-17 13:59:43 +0300

    method exercise

--- src/DeseneGore.java
+++ src/DeseneGore.java
@@ -1,35 +1,22 @@
 import java.util.Scanner;
 
 public class DeseneGore {
-    static int f() {
-        int result = 1;
-        int result2 = 1;
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int n1 = input.nextInt();
-
-        for (int i = n1; i > -n1 - i; i -= 5) {
-            result = i;
-            System.out.print(result + " ");
+    static int loop(int n) {
+        for (int i = n; i >= -4; i -= 5) {
+            System.out.print(i + " ");
             if (i <= 0) {
-                for (int k = i; k <= n1; k += 5) {
-                    result2 = k;
-                    System.out.print(result2 + " ");
+                for (int m = i; m <= n; m += 5) {
+                    System.out.print(m + " ");
                 }
             }
-            if (result2 == n1) {
-                break;
-            }
         }
-        System.out.println();
-        return f();
-
+        return n;
     }
+
     public static void main(String[] args) {
-
-        f();
-
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int k = input.nextInt();
+        loop(k);
     }
 }
