d10968b2d2867664483e8a9467f0dd6c56b27df0
Gokay Soysal <agokaysoysal@gmail.com>
2022-05-17 10:38:42 +0300

    method exercise

---
+++ src/MethodsFibo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MethodsFibo {
+    static int fib(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter the number to follow: ");
+        int k = input.nextInt();
+        System.out.print(fib(k));
+    }
+}
